9cd1ce9a30d8091e52cd591075a7123c84e3459f
khaueviana <khaueviana@multconnect.com.br>
2016-08-08 19:43:05 -0300

    first commit

---
+++ KV.Postsharp/KV.Postsharp.ConsoleApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KV.Postsharp.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
